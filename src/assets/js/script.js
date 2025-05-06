23edeb99a7fdbbc607302e51305d30e0e5deb521
albqvictor1508 <albq.victor@gmail.com>
2025-05-06 14:45:40 -0300

    wip: add minimal validation and messages for above the divs

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("form");
-form.onsubmit(() => {
+form.addEventListener("submit", e => {
+    e.preventDefault();
     init();
 })
 
@@ -7,5 +8,26 @@ form.onsubmit(() => {
 const init = () => {
     const usernameInput = document.querySelector("#username");
     const passwordInput = document.querySelector("#password");
-    console.log();
+    const formData = {usernameInput, passwordInput};
+    validateInputs(formData);
+}
+
+const validateInputs = ({usernameInput, passwordInput}) => {
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    if(username.length < 6) {
+        handleMessage("mto pequeno esse nome ai viado", usernameInput, true);
+    }
+
+    if(password.length < 6) {
+        handleMessage("mto pequeno essa senha ai viado", passwordInput, true);
+    }
+}
+
+const handleMessage = (msg, field, isError) => {
+    const div = document.createElement("div");
+    div.textContent = msg;
+    isError === true ? div.classList.add("error-message") : div.classList.add("error-message"); 
+    field.insertAdjacentElement("afterend",div);
 }
