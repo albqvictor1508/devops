e1166aae702e7be0322d58a4aa8957972a0eb47d
albqvictor1508 <albq.victor@gmail.com>
2025-05-06 14:29:42 -0300

    wip: initializating js

---
+++ src/assets/js/script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("form");
+form.onsubmit(() => {
+    init();
+})
+
+
+const init = () => {
+    const usernameInput = document.querySelector("#username");
+    const passwordInput = document.querySelector("#password");
+    console.log();
+}
