a01dade0ad1a4ed0c486c4d354b9d89ee2c7fe98
albqvictor1508 <albq.victor@gmail.com>
2025-05-06 14:51:21 -0300

    wip: messaging's working

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -28,7 +28,8 @@ const validateInputs = ({usernameInput, passwordInput}) => {
 const handleMessage = (msg, field, isError) => {
     const div = document.createElement("div");
     div.textContent = msg;
-    isError === true ? div.classList.add("message error-message") : div.classList.add("message safe-message"); 
+    div.classList.add("message")
+    isError === true ? div.classList.add("error-message") : div.classList.add("safe-message"); 
     field.insertAdjacentElement("afterend",div);
 }
 
