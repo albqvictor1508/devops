0ad72e21cda8e1cc3835070c449d786812a9609c
albqvictor1508 <albq.victor@gmail.com>
2025-05-08 08:55:55 -0300

    wip: checkout method idealized

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -9,20 +9,28 @@ const init = () => {
     const usernameInput = document.querySelector("#username");
     const passwordInput = document.querySelector("#password");
     const formData = {usernameInput, passwordInput};
-    validateInputs(formData);
+    checkout(formData);
 }
 
 const validateInputs = ({usernameInput, passwordInput}) => {
+    clearMessages();
+
+    let valid = true;
     const username = usernameInput.value;
     const password = passwordInput.value;
 
     if(username.length < 6) {
         handleMessage("mto pequeno esse nome ai viado", usernameInput, true);
+        valid = false;
     }
 
     if(password.length < 6) {
         handleMessage("mto pequeno essa senha ai viado", passwordInput, true);
+        valid = false
     }
+
+    if(!valid) return;
+    return {username, password};
 }
 
 const handleMessage = (msg, field, isError) => {
@@ -34,8 +42,27 @@ const handleMessage = (msg, field, isError) => {
 }
 
 const clearMessages = () => {
-    const allErrorMessages = document.querySelectorAll("message")
+    const allErrorMessages = document.querySelectorAll(".message")
     for(const msg of allErrorMessages) {
         msg.remove();
     }
+}
+
+const checkout = async (formData) => {
+    const validatedData = validateInputs(formData); 
+    
+    if(!validatedData) return; 
+
+    console.log({
+        username: validatedData.username,
+        password: validatedData.password
+    });
+    // const data = await fetch("", {
+    //     method: "POST",
+    //     headers: "Content-Type", //header de json, n sei qual é 
+    //     body: {
+    //         username: validatedData.username
+    //         password: validatedData.password
+    //     }
+    // })
 }
