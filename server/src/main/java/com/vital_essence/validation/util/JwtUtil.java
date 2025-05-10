ec2118d850e37e3eae3572ae0694c5a586aa1aed
albqvictor1508 <albq.victor@gmail.com>
2025-05-10 10:39:36 -0300

    wip: methods to manage jwt

--- server/src/main/java/com/vital_essence/validation/util/JwtUtil.java
+++ server/src/main/java/com/vital_essence/validation/util/JwtUtil.java
@@ -1,9 +1,14 @@
 package com.vital_essence.validation.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
 
@@ -21,5 +26,27 @@ public class JwtUtil {
 	private Key getSigningKey() {
 		var keyBytes = secret.getBytes(StandardCharsets.UTF_8);
 		return Keys.hmacShaKeyFor(keyBytes);
+	}
+
+	public String extractUsername(String token) {
+		return extractClaim(token, Claims::getSubject);
+	}
+
+	public Date extractExpiration(String token) {
+		return extractClaim(token, Claims::getExpiration);
+	}
+
+	public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+		final Claims claims = extractAllClaims(token);
+		return claimsResolver.apply(claims);
+	}
+
+	public Claims extractAllClaims(String token) {
+		return Jwts
+				.parserBuilder()
+				.setSigningKey(getSigningKey())
+				.build()
+				.parseClaimsJws(token)
+				.getBody();
 	}
 }
