33c422a39461e2361bede16dfef27eb1a89eb687
albqvictor1508 <albq.victor@gmail.com>
2025-05-10 10:47:43 -0300

    wip: generate token method

--- server/src/main/java/com/vital_essence/validation/util/JwtUtil.java
+++ server/src/main/java/com/vital_essence/validation/util/JwtUtil.java
@@ -1,13 +1,18 @@
 package com.vital_essence.validation.util;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -48,5 +53,31 @@ public class JwtUtil {
 				.build()
 				.parseClaimsJws(token)
 				.getBody();
+	}
+
+	public Boolean isTokenExpired(String token) {
+		return extractExpiration(token).before(new Date());
+	}
+
+	public String generateToken(UserDetails userDetails) {
+		Map<String, Object> claims = new HashMap<>();
+		return createToken(claims, userDetails.getUsername());
+	}
+
+	public String generateToken(Map<String, Object> claims, UserDetails userDetails) {
+		return createToken(claims, userDetails.getUsername());
+	}
+
+	private String createToken(Map<String, Object> claims, String subject) {
+		Date now = new Date();
+		Date expiryDate = new Date(now.getTime() + expiration);
+
+		return Jwts.builder()
+				.setClaims(claims)
+				.setSubject(subject)
+				.setIssuedAt(now)
+				.setExpiration(expiryDate)
+				.signWith(getSigningKey(), SignatureAlgorithm.HS256)
+				.compact();
 	}
 }
