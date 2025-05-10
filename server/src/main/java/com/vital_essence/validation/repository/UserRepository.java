c4bd58253e40ac54c261271a0c1eb6fc9c6fffbe
albqvictor1508 <albq.victor@gmail.com>
2025-05-10 02:01:48 -0300

    wip: user repository

---
+++ server/src/main/java/com/vital_essence/validation/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.vital_essence.validation.repository;
+
+import com.vital_essence.validation.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
