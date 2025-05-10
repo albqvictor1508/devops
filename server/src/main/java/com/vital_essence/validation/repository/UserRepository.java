e44a5120525d4526c011f97d7e9aa406f550261e
albqvictor1508 <albq.victor@gmail.com>
2025-05-10 14:39:15 -0300

    wip: changes on classes to put on database

--- server/src/main/java/com/vital_essence/validation/repository/UserRepository.java
+++ server/src/main/java/com/vital_essence/validation/repository/UserRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT * FROM users u WHERE u.username = :username", nativeQuery = true)
-    public User findByUsername(@Param("username") String username);
+    public Optional<User> findByUsername(@Param("username") String username);
     public boolean existsByUsername(String username);
 }
--- server/src/main/java/com/vital_essence/validation/security/CustomUserDetailsService.java
+++ server/src/main/java/com/vital_essence/validation/security/CustomUserDetailsService.java
@@ -1,27 +1,27 @@
 package com.vital_essence.validation.security;
 
+import com.vital_essence.validation.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.userdetails.User;
+import com.vital_essence.validation.entity.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 @AllArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
     private final PasswordEncoder passwordEncoder;
-    private final Map<String, String> users = new HashMap<>();
+    private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String encodedPassword = users.get(username);
-        if(encodedPassword == null) {
-            throw new UsernameNotFoundException("USER NOT FOUNDED: " + username);
-        }
-
-        return new User(username, encodedPassword, new ArrayList<>());
+        User u = userRepository
+                .findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("USER NOT FOUNDED: " + username));
+        return new org.springframework.security.core.userdetails.User(
+                u.getUsername(),
+                u.getPassword(),
+                new ArrayList<>()
+        );
     }
 }
