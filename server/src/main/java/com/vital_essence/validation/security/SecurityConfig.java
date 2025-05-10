7e750c232d04c60f4496764c2da73495768e17f0
albqvictor1508 <albq.victor@gmail.com>
2025-05-10 11:10:14 -0300

    wip: classic security filter method created

--- server/src/main/java/com/vital_essence/validation/security/SecurityConfig.java
+++ server/src/main/java/com/vital_essence/validation/security/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.vital_essence.validation.security;
 
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -10,4 +15,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class SecurityConfig {
     private final JwtRequestFilter jwtRequestFilter;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/**").permitAll().anyRequest().authenticated())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
