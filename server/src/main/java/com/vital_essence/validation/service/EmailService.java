54ef10cc1da69edff3a514aa57a3e495f2543531
albqvictor1508 <albq.victor@gmail.com>
2025-05-12 10:10:06 -0300

    wip: email service initial commit

---
+++ server/src/main/java/com/vital_essence/validation/service/EmailService.java
@@ -0,0 +1,11 @@
+package com.vital_essence.validation.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+}
