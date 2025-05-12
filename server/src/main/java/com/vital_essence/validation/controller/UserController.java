012c016350692d4611f75d6b2c734a3012839eb0
albqvictor1508 <albq.victor@gmail.com>
2025-05-12 09:59:23 -0300

    wip: add more endpoints to user controller

--- server/src/main/java/com/vital_essence/validation/controller/UserController.java
+++ server/src/main/java/com/vital_essence/validation/controller/UserController.java
@@ -1,9 +1,7 @@
 package com.vital_essence.validation.controller;
 
-import com.vital_essence.validation.dto.CreateUserResponse;
 import com.vital_essence.validation.dto.ForgotPasswordRequest;
 import com.vital_essence.validation.entity.User;
-import com.vital_essence.validation.security.JwtRequestFilter;
 import com.vital_essence.validation.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +15,7 @@ public class UserController {
     private UserService service;
 
     @PostMapping("/password")
-    public ResponseEntity<?> changePassword(ForgotPasswordRequest request) throws BadCredentialsException  {
+    public ResponseEntity<?> changePassword(@RequestBody ForgotPasswordRequest request) throws BadCredentialsException  {
         try {
             return ResponseEntity.ok(service.changePassword(request));
         } catch (BadCredentialsException e) {
@@ -26,8 +24,30 @@ public class UserController {
     }
 
     @PostMapping("/code/{id}")
-    public ResponseEntity<?> sendCode(@PathVariable Long id) {
-        User u = service.findById(id);
-        u.set
+    public ResponseEntity<?> sendCode(@RequestBody String email) {
+        User u = null;
+        try {
+            u = service.findByEmail(email);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("ERROR TO FIND USER: " + e);
+        }
+        service.sendCode(u);
+        return ResponseEntity.status(200).build();
+    }
+
+    @PostMapping("/code/{id}")
+    public ResponseEntity<?> receiveCode(@RequestBody String email, @RequestBody Integer code) {
+        User u = null;
+        try {
+            u = service.findByEmail(email);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("ERROR TO FIND USER: " + e);
+        }
+        try {
+        service.receiveCode(u, code);
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(400).body("ERROR TO RECEIVE CODE: " + e);
+        }
+        return ResponseEntity.status(200).build();
     }
 }
--- server/src/main/java/com/vital_essence/validation/service/UserService.java
+++ server/src/main/java/com/vital_essence/validation/service/UserService.java
@@ -5,6 +5,8 @@ import com.vital_essence.validation.entity.User;
 import com.vital_essence.validation.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,6 +27,24 @@ public class UserService {
 
     public User findById(Long id) {
         return userRepo.findById(id).orElseThrow(() -> new RuntimeException("User with this id not exist"));
+    }
+
+    public User findByEmail(String email) {
+        return userRepo.findByEmail(email).orElseThrow(() -> new RuntimeException("User with this email not exist"));
+    }
+
+    public void sendCode(User u) {
+        Integer code = 1000 + (int)(Math.random() * 9000);
+        u.setCode(code);
+        userRepo.save(u);
+        //enviar codigo pelo email do viado
+    }
+
+    public void receiveCode(User u, Integer code) {
+        if(!u.getCode().equals(code)) {
+            throw new BadCredentialsException("INVALID CODE");
+        }
+
     }
 
     public User save(User u) {
