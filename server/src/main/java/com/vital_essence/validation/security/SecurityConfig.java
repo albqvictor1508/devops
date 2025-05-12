55210a653d9875be9c8c161dd8981175b05c8556
albqvictor1508 <albq.victor@gmail.com>
2025-05-12 11:45:00 -0300

    chore: removing cors to avoid problems

--- server/src/main/java/com/vital_essence/validation/security/SecurityConfig.java
+++ server/src/main/java/com/vital_essence/validation/security/SecurityConfig.java
@@ -25,7 +25,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable())
+        http.cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/users/**").permitAll()
                         .requestMatchers("/auth/register").permitAll()
